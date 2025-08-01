decf2f83e0c4a4f65fda5d49407cac579e76bed7
Xavier <xavier@upstackstudio.com>
2025-08-02 03:39:34 +0800

    update db index checks

--- server/db/index.ts
+++ server/db/index.ts
@@ -19,7 +19,7 @@ type DrizzlePg = PostgresJsDatabase<typeof schema>;
 let db: DrizzleNeon | DrizzlePg
 
 
-if (config.NODE_ENV === 'development') {
+if (config.NODE_ENV === 'development' || config.NODE_ENV === 'test') {
   // console.log('Database connection established with schema:', schema);
 
   // Local PostgreSQL using pg Pool
@@ -34,7 +34,17 @@ if (config.NODE_ENV === 'development') {
 
   console.log('🟡 Using local PostgreSQL (pg) driver IN DEVELOPMENT MODE');
 
-} else if (config.NODE_ENV === 'production') {
+}
+// else if (config.NODE_ENV === 'test') {
+//   console.log('Database connection established in test mode');
+
+//   const sql = neon(process.env.DATABASE_URL!);
+//   db = drizzle(sql, { schema, logger: true });
+
+//   console.log('🟢 Using Neon (HTTP) driver IN TEST MODE');
+
+// } 
+else if (config.NODE_ENV === 'production') {
   console.log('Database connection established in production mode');
 
   const sql = neon(process.env.DATABASE_URL!);
